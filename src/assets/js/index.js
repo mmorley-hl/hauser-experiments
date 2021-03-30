393009a9d1d3381a76895e2547d90c620e9991ec
mmorley-hl <77000005+mmorley-hl@users.noreply.github.com>
2021-03-30 09:52:46 +0200

    Initial commit

---
+++ src/assets/js/index.js
@@ -0,0 +1,15 @@
+// import css for webpack
+import '../css/index.scss';
+// javscript imports
+import './_cursor.js';
+// import './_test.js';
+import './_header.js';
+import './_mapbox.js';
+import './_color_switch.js';
+import './_wow.js';
+import './_scrollAnker.js';
+import './_carousel.js';
+import './_toTopButton.js';
+import './_headroom.js';
+// import './_scrollTrigger.js';
+import './_dots.js';
